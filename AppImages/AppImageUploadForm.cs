524d4ba6e56e4307ae6909801287f284704b0218
Tom Sedlmeier <TomSedlmeier@hotmail.de>
2026-06-14 18:37:54 +0200

    chore: checkpoint current workspace state

--- AppImages/AppImageUploadForm.cs
+++ AppImages/AppImageUploadForm.cs
@@ -10,4 +10,19 @@ public sealed class AppImageUploadForm
 
     [FromForm(Name = "category")]
     public string Category { get; set; } = ImageCategories.Grasses;
+
+    [FromForm(Name = "capturedAtUtc")]
+    public DateTimeOffset? CapturedAtUtc { get; set; }
+
+    [FromForm(Name = "latitude")]
+    public double? Latitude { get; set; }
+
+    [FromForm(Name = "longitude")]
+    public double? Longitude { get; set; }
+
+    [FromForm(Name = "locationAccuracyMeters")]
+    public double? LocationAccuracyMeters { get; set; }
+
+    [FromForm(Name = "metadataSource")]
+    public string? MetadataSource { get; set; }
 }
--- Diagnostics/FieldDiagnosticAnalyzeForm.cs
+++ Diagnostics/FieldDiagnosticAnalyzeForm.cs
@@ -14,6 +14,21 @@ public sealed class FieldDiagnosticAnalyzeForm
     [FromForm(Name = "imageRoles")]
     public List<string> ImageRoles { get; set; } = [];
 
+    [FromForm(Name = "imageCapturedAtUtc")]
+    public List<string> ImageCapturedAtUtc { get; set; } = [];
+
+    [FromForm(Name = "imageLatitude")]
+    public List<string> ImageLatitude { get; set; } = [];
+
+    [FromForm(Name = "imageLongitude")]
+    public List<string> ImageLongitude { get; set; } = [];
+
+    [FromForm(Name = "imageLocationAccuracyMeters")]
+    public List<string> ImageLocationAccuracyMeters { get; set; } = [];
+
+    [FromForm(Name = "imageMetadataSource")]
+    public List<string> ImageMetadataSource { get; set; } = [];
+
     [FromForm(Name = "diagnosticRequestPubId")]
     public Guid? DiagnosticRequestPubId { get; set; }
 
